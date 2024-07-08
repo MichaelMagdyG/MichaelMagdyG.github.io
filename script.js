112e2ab70a721581fdcef680d4ac1fbf27f03abb
мιςнαεℓ <86680191+MichaelMagdyG@users.noreply.github.com>
2024-07-08 12:52:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,17 +15,16 @@ document.addEventListener("keydown", function (e) {
 });
 
 // Smooth scrolling for navigation links
-const navLinks = document.querySelectorAll('nav a');
+const navLinks = document.querySelectorAll("nav a");
 
-navLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault(); // Prevent default link behavior
-    const targetId = event.target.getAttribute('href');
+    const targetId = event.target.getAttribute("href");
     const targetElement = document.querySelector(targetId);
-    targetElement.scrollIntoView({ behavior: 'smooth' });
+    targetElement.scrollIntoView({ behavior: "smooth" });
   });
 });
 
 // Get current year for footer
-document.getElementById("currentYear").textContent =
-  new Date().getFullYear();
+document.getElementById("currentYear").textContent = new Date().getFullYear();
