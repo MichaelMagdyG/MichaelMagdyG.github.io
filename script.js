0078ca59328975df191ad01351736deaa56ea868
мιςнαεℓ <86680191+MichaelMagdyG@users.noreply.github.com>
2024-07-08 12:33:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-// Get all the navigation links
+// Disable right-click
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+  alert("Right Click is disabled on this website.");
+});
+
+// Disable Ctrl+C
+document.addEventListener("keydown", function (e) {
+  if (e.ctrlKey && (e.key === "c" || e.key === "C")) {
+    e.preventDefault();
+    alert("Copying is disabled on this website.");
+    // Optionally, you can prevent the alert from blocking further default action
+    return false;
+  }
+});
+
+// Smooth scrolling for navigation links
 const navLinks = document.querySelectorAll('nav a');
 
-// Add a click event listener to each link
 navLinks.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault(); // Prevent default link behavior
@@ -10,3 +25,7 @@ navLinks.forEach(link => {
     targetElement.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+// Get current year for footer
+document.getElementById("currentYear").textContent =
+  new Date().getFullYear();
